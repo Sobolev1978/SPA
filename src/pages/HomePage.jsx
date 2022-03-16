eadc9922c93e57a346695fd91e18c3efe7bdba93
SobolevVladimir <sobolev.vova2012@yandex.ru>
2022-03-16 11:00:25 +0300

    Add project

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import {useHistory} from "react-router-dom";
+
+import {Controls} from "../components/Controls";
+import {ALL_COUNTRIES} from "../config";
+import {List} from "../components/List";
+import {Card} from "../components/Card";
+
+export const HomePage = ({countries, setCountries}) => {
+  const [filtredCountries, setFiltredCountries] = useState([]);
+
+  const {push} = useHistory()
+
+  const handleSearch = (search, region) => {
+    let data = [...countries]
+
+    if(region) {
+      data = data.filter(c => c.region.includes(region))
+    }
+
+    if(search) {
+      data = data.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    setFiltredCountries(data)
+  }
+
+  useEffect(() => {
+    if(!countries.length)
+      axios.get(ALL_COUNTRIES).then(({data}) => setCountries(data))
+  }, []);
+
+  useEffect(() => {
+    handleSearch();
+  }, [countries])
+
+  return (
+    <>
+      <Controls onSearch={handleSearch} />
+      <List>
+        {
+          filtredCountries.map(c => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString()
+                },
+                {
+                  title: 'Region',
+                  description: c.region
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital
+                }
+              ],
+            }
+
+            return (
+              <Card key={c.name} onClick={() => push(`/country/${c.name}`)} {...countryInfo} />
+            )
+          })
+        }
+      </List>
+    </>
+  )
+}
